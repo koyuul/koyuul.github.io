9dd5db178242f8a6bda02b590689b5439f138844
koyuul <shannonkyle.deleon@gmail.com>
2022-02-22 09:27:25 -0800

    added url params on modal click

--- script.js
+++ script.js
@@ -1,134 +1,101 @@
-/*
-todo:
-    set close/open to work based off of link
-    apply to all
-
-    so im trying to iterate through both of them but its messed up so thats why its not working
-*/
-
 //#region modal stuff
-
-/*
-apply for all thought process:
-    - have a key value pair for each modal
-    - work through key value pair, doing the same process for each
-        - for each key val in modals
-            - for each key val in each obj
-            run process
-*/
-
-modal_dicts = {
-    tt: {
+let modal_dicts = {
+    typetester: {
         modal: document.getElementById("tt_modal"),
         btn: document.getElementsByClassName("tt_open"),
-        close: document.getElementById("tt_close")
+        close: document.getElementById("tt_close"),
+        link_arg: "typetester",
     },
 
     twit: {
         modal: document.getElementById("twit_modal"),
         btn: document.getElementsByClassName("twit_open"),
-        close: document.getElementById("twit_close")
+        close: document.getElementById("twit_close"),
+        link_arg: "2021percent",
+
     },
 
     cssi: {
         modal: document.getElementById("cssi_modal"),
         btn: document.getElementsByClassName("cssi_open"),
-        close: document.getElementById("cssi_close")
+        close: document.getElementById("cssi_close"),
+        link_arg: "cssi",
     },
 
     graduation: {
         modal: document.getElementById("graduation_modal"),
         btn: document.getElementsByClassName("graduation_open"),
-        close: document.getElementById("graduation_close")
+        close: document.getElementById("graduation_close"),
+        link_arg: "graduation",
     },
 
     napalearns: {
         modal: document.getElementById("napalearns_modal"),
         btn: document.getElementsByClassName("napalearns_open"),
-        close: document.getElementById("napalearns_close")
+        close: document.getElementById("napalearns_close"),
+        link_arg: "napalearns",
+    }
+};
+
+const params = new URLSearchParams(window.location.search);
+let link_modal = params.get('modal');
+let open_modal_id = null;
+
+let show = function(modal_id, refreshLink=true){ 
+    modal_dicts[modal_id]['modal'].style.display = "block";
+    document.body.style.overflow = "hidden";
+
+    params.set('modal', modal_dicts[modal_id]['link_arg']);
+    if (refreshLink === true){
+        window.history.replaceState({}, '', `?${params}`);
+    }
+
+    open_modal_id = modal_id;
+}
+
+let hide = function(modal_id){
+    modal_dicts[modal_id]['modal'].style.display="none";
+    document.body.style.overflow = "visible";
+
+    params.delete('modal');
+    console.log(params.toString());
+    window.history.replaceState({}, '', `?`);
+
+    open_modal_id = null;
+}
+
+if (link_modal){ 
+    show(link_modal, false); //opens without refreshing URL
+}
+
+//applies onclick events to each of card's elements to open, and onclick events to each 'X' element to close 
+for(let modal_id in modal_dicts){
+    for(let btn of modal_dicts[modal_id]["btn"]){ // sets any part of modal area to open it
+        btn.onclick = function(){
+            show(modal_id);
+        };
+    }
+
+    modal_dicts[modal_id]["close"].onclick = function(){ //closes on X button click button
+        hide(modal_id);
     }
 }
 
 
-
-for(let topic in modal_dicts){
-    for(let element in modal_dicts[topic]){
-        for(let btn of modal_dicts[topic]["btn"]){
-            btn.onclick = function(){
-                console.log(modal_dicts[topic]["modal"])
-                modal_dicts[topic]["modal"].style.display = "block";
-                document.body.style.overflow = "hidden";
-            }
+document.addEventListener('keyup', (event) => { //closes on escape key press
+    if(open_modal_id){
+        if(event.code === "Escape"){
+            hide(open_modal_id);
         }
-
-        document.addEventListener('keydown', (event) => {
-            if(event.keyCode == 27){
-                modal_dicts[topic]["modal"].style.display="none";
-                document.body.style.overflow = "visible";
-            }
-        })
-
-        modal_dicts[topic]["close"].onclick = function(){
-            modal_dicts[topic]["modal"].style.display = "none";
-            document.body.style.overflow = "visible";
-        }
-        
-        document.addEventListener('mousedown', (event) => {
-            if(event.target == modal_dicts[topic]["modal"]){
-                modal_dicts[topic]["modal"].style.display="none";
-                document.body.style.overflow = "visible";
-            }
-        })
     }
-}
-// var tt_modal = document.getElementById("tt_modal")
-// var tt_btn = document.getElementsByClassName("tt_open")
-// var tt_close = document.getElementById("tt_close");
-
-
-// window.onmousedown = function(event){
-//     if(event.target == tt_modal){
-//         tt_modal.style.display = "none";
-//         document.body.style.overflow = "scroll";
-
-//     }
-// }
-
-
-// var twit_modal = document.getElementById("twit_modal")
-// var twit_btn = document.getElementsByClassName("twit_open")
-// var twit_close = document.getElementById("twit_close");
-
-// for(let element of twit_btn){
-//     element.onclick = function(){
-//         twit_modal.style.display = "block";
-        
-//         new Glide('.glide').mount();
-//         document.body.style.overflow = "hidden";
-//     }
-// }
-
-
-// document.addEventListener('keydown', (event) => {
-//     if(event.keyCode == 27){
-//         twit_modal.style.display="none"
-//         document.body.style.overflow = "scroll";
-//     }
-// })
-
-// twit_close.onclick = function(){
-//     console.log('test')
-//     twit_modal.style.display = "none";T
-//     document.body.style.overflow = "scroll";
-// }
-
-// window.onmousedown = function(event){
-//     if(event.target == twit_modal){
-//         twit_modal.style.display = "none";
-//         document.body.style.overflow = "scroll";
-
-//     }
-// }
+})
+document.addEventListener('mousedown', (event) => { //closes on outside click
+    if(open_modal_id){
+        if(event.target == modal_dicts[open_modal_id]["modal"]){
+            hide(open_modal_id);
+        }
+    }
+})
 
 
 // //#endregion
