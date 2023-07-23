fdd2b8f35ea928f778e9985ff6c1b64e211e3816
koyuul <shannonkyle.deleon@gmail.com>
2023-07-22 21:30:25 -0700

    updated experience to include nasa work

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import ExperienceCard from "./ExperienceCard";
 import "./Experience.css";
-import napaLearns from "../../Assets/Cards/napaLearns/napaLearns.json";
+import nasa from "../../Assets/Cards/nasa/nasa.json";
 import cssi from "../../Assets/Cards/cssi/cssi.json";
 import spaceLab from "../../Assets/Cards/spaceLab/spaceLab.json";
 import cube3 from "../../Assets/Cards/cube3/cube3.json";
@@ -13,16 +13,16 @@ export default () => {
         <div id="experience">
             <div id="experience-content">
             <div id="experience-top-row" className="experience-row">
-                <ExperienceCard experienceFile={napaLearns}/>
-                <ExperienceCard experienceFile={spaceLab}/>
+                <ExperienceCard experienceFile={cssi}/>
+                <ExperienceCard experienceFile={cube3}/>
                 
             </div>
             
             <div id="experience-timeline"/>
             
             <div id="experience-bottom-row" className="experience-row">
-                <ExperienceCard experienceFile={cssi}/>
-                <ExperienceCard experienceFile={cube3}/>
+                <ExperienceCard experienceFile={spaceLab}/>
+                <ExperienceCard experienceFile={nasa}/>
             </div>
             </div>
         </div>
--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,8 @@ import "./index.css";
 ReactDOM.render(
     <BrowserRouter>
         <App/>
+        <link rel="preload" href="https://i.ibb.co/PNB09Q6/a.png" as="image"/>\
+        <link rel="preload" href="https://i.ibb.co/PCHKM27/a.png" as="image"/>\
         <link rel="preload" href="https://i.ibb.co/YL9kf8p/background-min.jpg" as="image"/>\
         <link rel="preload" href="https://i.ibb.co/9wtFQFx/avatar.png" as="image"/>\
         <link rel="preload" href="https://i.ibb.co/wrXwd10/streetkombat.png" as="image"/>
